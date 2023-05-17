0c6abe94be943dd0340d728387f497e8eac83b5c
Moraaalice <124131335+Moraaalice@users.noreply.github.com>
2023-05-17 22:20:54 +0300

    Update customer.py

--- customer.py
+++ customer.py
@@ -1,17 +1,31 @@
-# customer
-class Customer:
-    def __init__(self,name,phone_number,email,address):
-        self.name = name
+#cart
+class Cart:
+    def __init__(self, item, phone_number, payment,address):
+        self.name = item
         self.phone_number = phone_number
-        self.email = email
+        self.email = payment
         self.address = address
         self.cart = []
-#I have created an empty list so the customer can be able to append to the empty list every time they
-#purchase goods
-    def add_to_cart(self,*item):
+
+    def add_to_cart(self, item):
         self.cart.append(item)
-        print(self.cart)
-    def remove_from_cart(self,item):
+
+    def remove_from_cart(self, item):
         if item in self.cart:
             self.cart.remove(item)
-            print(self.cart)
+
+    def view_cart(self):
+        if len(self.cart) == 0:
+            print("Your cart is empty.")
+        else:
+            print("Your cart contains:")
+            for item in self.cart:  
+                    print("- " + item)
+
+# Example objects
+customer1 = Cart(item="Mangoes",phone_number="0765433289",payment="cash on delivery",address=56788)
+
+# Example usage
+customer1.add_to_cart("Apples")
+customer1.add_to_cart("Bananas")
+customer1.view_cart()
