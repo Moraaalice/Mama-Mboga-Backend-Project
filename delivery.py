a5bb12dfc9bbf95ef1b6f9f2b3cf559867b176e1
njugunagrace <124131619+njugunagrace@users.noreply.github.com>
2023-05-17 14:19:52 +0300

    added methods that allow a customer to add items to cart remove and choose delivery options

--- delivery.py
+++ delivery.py
@@ -1,12 +1,21 @@
 class Order:
     def __init__(self, deliveryOptions):
         self.deliveryOptions = deliveryOptions
+        products=[]
+    def add_product(self, productName, productPrice):
+        self.products.append((productName, productPrice))
+    def calculate_total(self):
+        total = 0
+        for product in self.products:
+            total += product[1]
+        return total
+    def removeProduct(self, product):
+        self.products.remove(product)
     def getDeliveryOptions(self):
-        return self.deliveryOptions
-    def calculate_total(product_price):
-        sum=0
-        for price in product_price:
-            sum+=price
+        return self.deliveryOptions 
+      
+ 
+
             
     
     
