d013a5cc497e0d67732bf444636c9ffa708977d3
Moraaalice <124131335+Moraaalice@users.noreply.github.com>
2023-05-17 22:37:24 +0300

    Update delivery.py

--- delivery.py
+++ delivery.py
@@ -1,7 +1,10 @@
 class Order:
-    def __init__(self, deliveryOptions):
+    def __init__(self, deliveryOptions,location,driver):
         self.deliveryOptions = deliveryOptions
+        self.location = location
+        self.driver = driver
         products=[]
+        
     def add_product(self, productName, productPrice):
         self.products.append((productName, productPrice))
     def calculate_total(self):
@@ -9,19 +12,20 @@ class Order:
         for product in self.products:
             total += product[1]
         return total
+    
     def removeProduct(self, product):
         self.products.remove(product)
+        
     def getDeliveryOptions(self):
         return self.deliveryOptions 
       
- 
+ order = Order(deliveryOptions, location, driver)
+order.add_product("Product 1", 10.99)
+order.add_product("Product 2", 5.99)
 
-            
-    
-    
-    
-    
-    
-order = Order( [ "Home Delivery", "Pickup point"])
-deliveryOptions = order.getDeliveryOptions()
-print(deliveryOptions)
+total = order.calculate_total()
+print("Total: $", total)
+
+order.removeProduct(("Product 1", 10.99)) options = order.getDeliveryOptions()
+print("Delivery options:", options)
+
