a9be5c61c483423c0001a7c19daba8b7c9af4510
Moraaalice <124131335+Moraaalice@users.noreply.github.com>
2023-05-17 22:05:12 +0300

    Updated my class payment by importing user class,product class and order class so that i can be able to know the delivery options my customer can have and mama mboga to see the payment details of the customer

--- payment.py
+++ payment.py
@@ -1,4 +1,63 @@
 
+class User:
+    def __init__(self, username, email, password, firstName, lastName,confirm_password, phoneNumber):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.confirm_password=confirm_password
+        self.firstName = firstName
+        self.lastName = lastName
+        self.phoneNumber = phoneNumber
+        
+                    
+    def get_username(self):
+        return self.username  
+    
+    def get_email(self):
+        return self.email  
+    
+    def get_first_Name(self):
+        return self.firstName  
+    
+    def get_lastName(self):
+        return self.lastName 
+    
+    def phone_number(self):
+        return self.phoneNumber
+     
+    def get_password(self):
+        return self.password
+    
+user1 = User(username="Lisa",email="lisa@gmail.com",password=567892,firstName="Alicia",lastName="Njer",confirm_password=567892,phoneNumber="07654321")
+username = user1.get_username()
+print(username)
+email = user1.get_email()
+print(email)
+firstname = user1.get_first_Name()
+print(firstname)
+lastName = user1.get_lastName()
+print(lastName)
+phoneNumber = user1.phone_number()
+print(phoneNumber)
+password = user1.get_password()
+print(password)
+
+
+
+class Product:
+    def __init__(self,name,price,category,stock):
+        self.name = name
+        self.price = price
+        self.category = category
+        self.stock = stock
+        
+        
+class Order:
+    def __init__(self, deliveryOptions,location,driver):
+        self.location = location
+        self.deliveryOptions = deliveryOptions
+        self.driver = driver
+        
 class Payments:
     def __init__(self, order, method, amount):
         self.order = order
@@ -6,14 +65,22 @@ class Payments:
         self.amount = amount
 
     def process_payment(self):
-        print("Payment processing")
+        print("Processing payment...")
+        # Code to process payment here
+        print("Payment processed successfully.")
 
-    def processed_payment(self):
-        print("Payments processed")
+    def view_info(self):
+        print("Payment Details:")
+        print(f"Amount: ${self.amount:.2f}")
+        print(f"Method: {self.method}")
 
-payment1 = Payments(order="oranges", method="mpesa", amount=1000)
+# Example objects
+product1 = Product(name="Kales",price=35,category="vegetable",stock=200)
+customer1 = User(username="Johnte",email="johnny@gmail.com",password=45677,firstName="Johnny",lastName="Mwangi",confirm_password=45677,phoneNumber="074536254")
+order1 = Order(deliveryOptions="motorbike",location="Karen",driver="Mwangi Paul")
+
+payment1 = Payments(order1, "Credit Card", 2.50)
+
+payment1.view_info()
+
 payment1.process_payment()
-
-
-payment2 = Payments(order="carrots", method="mpesa", amount=300)
-payment3 = Payments(order="mangoes", method="account number", amount=200)
